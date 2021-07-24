ca68e11316374862070acab9b244cbdc238cb172
sanggonlee <lee.sg16@gmail.com>
2021-07-24 09:52:33 -0400

    .

--- dummy.go
+++ dummy.go
@@ -1,9 +1,5 @@
 package dummy_go
 
-// import dummygo "github.com/sanggonlee/dummy_go/haha"
+import dummygo "github.com/sanggonlee/dummy_go/haha"
 
-// var Haha = dummygo.Haha
-
-func Haha() string {
-	return "hahaha"
-}
+var Haha = dummygo.Haha
